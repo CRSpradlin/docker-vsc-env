7214265b06abdb8784c4ad86dc40ab5a8e3779cc
crspradlin <mail@crspradlin.org>
2020-11-07 12:39:50 -0500

    updated readme and runvnc.sh to have direct follow link to NoVNC server

---
+++ README.md
@@ -0,0 +1,4 @@
+## Installation
+
+## Running GUI Applications
+Once you have the docker container running and are in the code-server via `localhost`, try running `bash runvnc.sh` to start the service. You may need to type your sudo password that you specified in the Dockerfile.
--- workspace/runvnc.sh
+++ workspace/runvnc.sh
@@ -3,4 +3,4 @@
 vnc4server :0 < /config/vnc/password &>>/dev/null&
 /config/vnc/noVNC/utils/launch.sh --listen 8080 &>>/dev/null&
 
-echo VNC Server is now running at port 8080, visit localhost:8080 in the browser to view gui applications.
+echo VNC Server is now running at port 8080, visit http://localhost:8080/vnc_lite.html in the browser to view gui applications.
