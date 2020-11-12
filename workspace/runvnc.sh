d0a069e3bc6b1f0595afd992e055ace5b3e564ff
crspradlin <mail@crspradlin.org>
2020-11-12 00:45:03 -0500

    added minimal window manager

--- workspace/runvnc.sh
+++ workspace/runvnc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
+echo Starting VNC...
 vnc4server :0 < /config/vnc/password &>>/dev/null&
 /config/vnc/noVNC/utils/launch.sh --listen 8080 &>>/dev/null&
+sleep 5
+x-window-manager &
 
 echo VNC Server is now running at port 8080, visit http://localhost:8080/vnc_lite.html in the browser to view gui applications.
